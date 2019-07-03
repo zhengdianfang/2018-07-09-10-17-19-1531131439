3804bb1e5fe9b669a4c16046c4816b4aa8ee29bd
Dianfang Zheng <dfgzheng@thoughworks.com>
2019-07-03 14:11:20 +0800

    1. finish practice09 implement

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,44 @@
 package practice09;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Klass {
+    private int number;
+
+    private Student leader;
+
+    private List<Student> members = new ArrayList<>();
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public List<Student> getMembers() {
+        return members;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public String getDisplayName() {
+        return String.format("Class %d", this.number);
+    }
+
+    public void assignLeader(Student leader) {
+        if (!this.members.contains(leader)) {
+            System.out.println("It is not one of us.");
+           return;
+        }
+        this.leader = leader;
+    }
+
+    public void appendMember(Student student) {
+        members.add(student);
+    }
 }
--- src/main/java/practice09/Person.java
+++ src/main/java/practice09/Person.java
@@ -1,4 +1,44 @@
 package practice09;
 
+import java.util.Objects;
+
 public class Person {
+    private int id;
+    private String name;
+    private int age;
+
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String introduce() {
+        return String.format("My name is %s. I am %d years old.", this.getName(), this.getAge());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Person person = (Person) o;
+
+        if (id != person.id) return false;
+        if (age != person.age) return false;
+        return Objects.equals(name, person.name);
+    }
+
 }
